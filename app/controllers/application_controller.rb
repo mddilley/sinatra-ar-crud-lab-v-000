39532ed901ba427153c375353b8dd99c05e32be8
Mike Dilley <mike.dilley@gmail.com>
2018-12-18 16:09:42 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,39 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    @article = Article.create("title" => params[:title], "content" => params[:content])
+    redirect "/articles/#{Article.last.id}"
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @article = Article.update("title" => params[:title], "content" => params[:content])
+    erb :show
+  end
+
+  delete '/articles/:id' do
+    Article.destroy(params[:id])
+    erb :index
+  end
+
 end
